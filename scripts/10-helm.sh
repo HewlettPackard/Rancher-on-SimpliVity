31ab300cd09c7c94029ff4e2a8e99691839c023d
Christophe Le Hy <christophe.lehy@hpe.com>
2020-02-21 10:48:03 +0000

    More stuff

--- scripts/10-helm.sh
+++ scripts/10-helm.sh
@@ -1,2 +1,3 @@
+#requirs proxy
 helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
 kubectl create namespace cattle-system
--- scripts/30-rancher.sh
+++ scripts/30-rancher.sh
@@ -1,5 +1,12 @@
+setfile=$(mktemp)
+cat <<EOF >${setfile}
+hostname: lb1.clh.org
+proxy: http://10.12.7.21:8080
+noProxy: 127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,.am2.cloudra.local,.clh.org,10.15.152.0/24
+EOF
+
 helm install rancher rancher-stable/rancher \
   --namespace cattle-system \
-  --set hostname=lb1.clh.org
+  --values=${setfile} 
 
 kubectl -n cattle-system rollout status deploy/rancher
