86291687ef73f7d5ee46f285840ed0855998e710
Doug Lamoureux <dougl@hpe.com>
2020-04-29 16:34:13 -0700

    Remove script dir

--- scripts/cpi/20_ccm_verify.sh
+++
@@ -1,4 +0,0 @@
-rancher kubectl describe CSINode
-rancher kubectl get pods --namespace=kube-system -o wide
-rancher kubectl describe nodes | grep ProviderID
-rancher kubectl describe nodes | egrep "Taints:|Name:"
